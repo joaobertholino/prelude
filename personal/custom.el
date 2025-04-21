e248a806e8f6760ff00f2a76df490a385d5c8ebc
João Bertholino <comercial.bertholino@gmail.com>
2025-04-21 20:04:29 -0300

    Add new theme and hide menu top bar and scrol bar

---
+++ personal/custom.el
@@ -0,0 +1,24 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(cua-mode t)
+ '(custom-enabled-themes '(tomorrow-night-paradise))
+ '(custom-safe-themes
+   '("4aafea32abe07a9658d20aadcae066e9c7a53f8e3dfbd18d8fa0b26c24f9082c"
+     default))
+ '(global-display-line-numbers-mode t)
+ '(scroll-bar-mode nil)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(menu-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
